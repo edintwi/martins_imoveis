c4b3fffe31384437e7f639f044f22aef4f20ad5f
Gabriell Maia <gabriell.maia@outlook.com>
2021-10-02 15:58:15 -0300

    Mudança de página 2.0

--- casas.php
+++ casas.php
@@ -83,7 +83,7 @@ session_start();
 
     
     <footer id="footer-info">
-        Rua Rio da Prata, 245, Sala 105, Bangu | Tel - 2401-2142 |Cel - 98899-2124 / 98755-2142 / 2401-2142
+        <p>Rua Rio da Prata, 245, Sala 105, Bangu | Tel - 2401-2142 |Cel - 98899-2124 / 98755-2142 / 2401-2142</p>
         </footer>
        
     
