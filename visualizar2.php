9b899c881b1a67c925193c90ddfe782ffe4c8d74
Davi Moura <foreverfacil@gmail.com>
2021-10-07 12:01:42 -0300

    Auto stash before merge of "main" and "origin/main"

---
+++ visualizar2.php
@@ -0,0 +1,102 @@
+<?php 
+  include "include/header.php";
+  include('scripts/connection.php');
+  include('scripts/view.php');
+?>
+     
+    <section class="container"> 
+        <div class="text-container w-50">
+          <h1><?php echo $dados['titulo'] ?></h1>
+          <h2>Bairro: <?php echo $dados['bairro'] ?></h2>
+           <p style="white-space: break-spaces;"><?php echo $dados['descricao']  ?></p>
+            <h2>Endereço: </h2>
+            <h2>Valor: R$ <?php echo number_format($dados['valor'],2,",","."); ?></h2>
+            <a href=" https://api.whatsapp.com/send?phone=5521993576574&text=Ol%C3%A1%2C%20tenha%20interesse%20na%20casa%2Fapartamento%20de%20c%C3%B3digo%3A%20%24%7Bcodigo%7D!">Entre em contato</a>
+        </div>
+
+
+<!-- Images used to open the lightbox -->
+<div class="row w-50">
+  <?php $dir = $dados['diretorio_thumb'];
+                    $files = scandir($dir); 
+                      
+                ?>     
+                <?php foreach ($files as $file){ ?>
+                    <?php $caminho = $dir."/".$file?>
+                    <?php if ($file != '.' && $file != '..'){ ?>
+                    <img class="img-bom" src="<?php echo $caminho ?>">    
+                <?php }}; ?>  
+    </div>
+   
+  
+</div>
+
+<!-- The Modal/Lightbox -->
+<div id="myModal" class="modal">
+  
+  <span class="close cursor" onclick="closeModal()">&times;</span>
+  
+    <div class="modal-content">
+    <?php foreach ($files as $file){ ?>
+  <?php $caminho = $dir."/".$file?>
+  <?php if ($file != '.' && $file != '..'){ ?> 
+    <div class="mySlides">
+        <div class="numbertext">1 / 4</div>
+        <img src="<?php echo $caminho ?>" style="width:100%">
+      </div>
+      <?php }}; ?>   
+  </div>
+  
+  
+      </section> 
+
+  </div>
+</div>
+
+
+    </section>
+
+    <script>
+        // Open the Modal
+        function openModal() {
+        document.getElementById("myModal").style.display = "block";
+        }
+
+        // Close the Modal
+        function closeModal() {
+        document.getElementById("myModal").style.display = "none";
+        }
+
+        var slideIndex = 1;
+        showSlides(slideIndex);
+
+        // Next/previous controls
+        function plusSlides(n) {
+        showSlides(slideIndex += n);
+        }
+
+        // Thumbnail image controls
+        function currentSlide(n) {
+        showSlides(slideIndex = n);
+        }
+
+        function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("demo");
+        var captionText = document.getElementById("caption");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " active";
+        captionText.innerHTML = dots[slideIndex-1].alt;
+        }
+    </script>
+
+<?php include "include/footer.php"; ?>
